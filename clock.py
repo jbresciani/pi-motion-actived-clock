152c1657093e268f8e61b163bd6236830188fda5
Jacob Bresciani <jbresciani@xmatters.com>
2021-01-08 13:44:59 -0800

    add requirements.txt file, add example service startup script, adjust code to verify the distance to reduce sensor glitchs and add a timeout to the sensor read to hopefully reduce lockups

--- clock.py
+++ clock.py
@@ -4,24 +4,29 @@ import RPi.GPIO as GPIO
 import tm1637
 import datetime
 import time
+import timeout_decorator
 
 # setup the tm1637 4 digit 7 segment display
 tm = tm1637.TM1637(clk=6, dio=5)
 
-#GPIO Mode (BOARD / BCM)
+# GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
  
-#set GPIO Pins
+# set GPIO Pins
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
  
-#set GPIO direction (IN / OUT)
+# set GPIO direction (IN / OUT)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
-
 debug = False
 
 def display_time(turn_on):
+    """ display the time on the tm1637 display
+
+    Inputs:
+        turn_on (bool) - True will display the current time for 10 seconds, False will clear the display
+    """
     if debug:
         print(f"turn_on = {turn_on}")
     if turn_on:
@@ -36,8 +41,15 @@ def display_time(turn_on):
     else:
         tm.write([0, 0, 0, 0])
 
+@timeout_decorator.timeout(2)
+def detect_object(object_closer_then):
+    """Check if the sensor is reading any objects within a given distance.
 
-def distance():
+    Inputs
+        object_closer_then (int) - the distance in CM that an object should be closer then
+    Returns
+        Boolean, True if distance < object_closer_then, False if it is not
+    """
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
  
@@ -61,16 +73,27 @@ def distance():
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
+    if debug:
+        print ("Measured Distance = %.1f cm" % distance)
+
+    return distance < object_closer_then
+
+
 if __name__ == '__main__':
+    print("Starting motion-clock v1.0")
     try:
         while True:
-            dist = distance()
-            if debug:
-                print ("Measured Distance = %.1f cm" % dist)
-            display_time(dist < 45)
+            turn_on = False
+            if detect_object(45):
+                turn_on = True
+                #confirm it's still there 3 more times
+                for x in range(0,3):
+                    time.sleep(0.1)
+                    print(f"verifying detection, attempt {x}")
+                    if not detect_object(45):
+                        turn_on = False
+                        break
+            display_time(turn_on)
             time.sleep(0.25)
  
     except KeyboardInterrupt:
